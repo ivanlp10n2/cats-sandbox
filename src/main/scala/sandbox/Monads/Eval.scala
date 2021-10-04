2d04f09dec6a7b62f90ee64376991f29b204242a
ivan <imonzon-rb@redb.ee>
2021-10-04 04:38:20 -0300

    reader, writer and eval

--- src/main/scala/sandbox/Monads/Eval.scala
+++ src/main/scala/sandbox/Monads/Eval.scala
@@ -2,6 +2,7 @@ package sandbox.Monads
 
 object EvalTheory extends App{
   import cats.Eval
+
   val saying = Eval
     .always(println("doin always"))
     .map(_ => println("then do something and memoize these results"))
@@ -12,6 +13,53 @@ object EvalTheory extends App{
   saying.value
   saying.value
 }
+
+object ValidateApp extends App{
+  def validateAdult(age: Int): Boolean=
+    (age >= 18)
+
+  import cats.MonadError
+  def validateAdult[F[_]](age: Int)(implicit me: MonadError[F, Throwable]): F[Int] =
+    me.ensure(me.pure(age))(new IllegalArgumentException("no es mayor"))(age => age >= 18)
+//    if (age >= 18) me.pure(age)
+//    else me.raiseError(new IllegalArgumentException("no es mayor"))
+
+
+    import scala.Either
+    type EitherOr[A] = Either[Throwable, A]
+    println(validateAdult[EitherOr](18))
+}
+
 object StackSafety extends App{
-  
+  def factorial(n: BigInt): BigInt =
+    if (n == 1) n
+//    else factorial(n - 1, n * acc)
+        else n * factorial(n - 1)
+
+    println(factorial(40000))
+
+  import cats.Eval
+  def factorialEval(n: BigInt): Eval[BigInt] =
+    if (n == 1) Eval.now(n)
+    else Eval.defer(factorialEval(n - 1))
+      .map(_ * n)
+
+//    println(factorialEval(40000).value)
+
+
+  def foldRight[A, B](as: List[A], acc: B)
+                     (f: (A, B) => B) : B =
+    foldRightEval(as, Eval.now(acc))( (a,b) =>
+      b.map( f(a, _))
+    ).value
+
+  def foldRightEval[A,B](as: List[A], acc: Eval[B])
+                        (f: (A, Eval[B]) => Eval[B]): Eval[B] =
+    as match {
+      case head :: tail =>
+        Eval.defer( foldRightEval(tail, f(head, acc))(f))
+      case Nil =>
+        acc
+    }
+
 }
---
+++ src/main/scala/sandbox/Monads/Writter.scala
@@ -0,0 +1,49 @@
+package sandbox.Monads
+
+object Writter extends App{
+  import cats.data.Writer
+  val log = Writer(Vector(
+    "There was upon a time",
+    "Did not worked"
+  ), 1900)
+  println(log)
+
+  import cats.instances.vector._
+//  implicit val vectorMonoid: Monoid[Vector] = new Monoid[Vector]{
+//    override def empty: Vector = Vector.empty
+//
+//  }
+  val a = Writer.value(1900)
+  println(a)
+
+  def slowly[A](body: => A) =
+    try body finally Thread.sleep(100)
+
+  import cats.syntax.applicative._
+  import cats.instances.vector._
+  type Logged[A] = Writer[Vector[String], A]
+  def factorial(n: Int): Logged[Int] = {
+    import cats.syntax.writer._
+    for{
+      res <-
+          if (n == 1) n.pure[Logged]
+          else slowly(
+            factorial(n - 1)
+              .map(_ * n)
+          )
+      _  <- Vector(s"fact $n: $res ").tell
+    } yield res
+  }
+
+  import scala.concurrent.Await
+  import scala.concurrent.Future
+  import scala.concurrent.duration._
+  implicit val ec = scala.concurrent.ExecutionContext.global
+  val logs = Await.result(
+    Future.sequence(Vector(
+      Future(factorial(5)),
+      Future(factorial(5))
+    ).map(f => f.map(log => log.written.reverse))), 5.seconds)
+
+  println(logs)
+}
