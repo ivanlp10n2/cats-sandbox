4759a4150c7a9a41d31aaee724e1d58868fc5910
ivan <imonzon-rb@redb.ee>
2021-10-08 22:14:00 -0300

    not finished but close

---
+++ src/main/scala/sandbox/CaseStudy/CRDTs.scala
@@ -0,0 +1,16 @@
+package sandbox.CaseStudy
+
+object CRDTs extends App{
+
+  final case class GCounter(counters: Map[String, Int]){
+    import cats.syntax.semigroup._
+    def increment(machine: String, amount: Int) =
+      counters.get(machine).map(n  => n |+| amount)
+
+    def merge(that: GCounter): GCounter =
+      that.
+
+    def total: Int = counters.values.sum
+  }
+
+}
